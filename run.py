53a405a1989665e5f825ba7ec8f9a7b18825ea15
gajkez <125378585+LukaGajic2@users.noreply.github.com>
2023-05-22 09:48:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -151,6 +151,24 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    return stock_data
+
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+stock_data = main()
+
+
+# Write you code below this comment
+
+
+def get_stock_values(data):
+    print('Make the following numbers of sandwiches for next market:\n')
+    
+    headings = SHEET.worksheet('stock').row_values(1)
+    #print(headings)
+    
+    return{heading: data for heading, data in zip(headings,data)}
+    
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
